8dc4460eb6ae73ac6eb3d7c333d4f65722c723d3
Quynh-Truong <quynh88@hotmail.com>
2023-10-21 21:29:35 +0200

    Add project files.

---
+++ CircleArea/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    internal class Circle
+    {
+        public int Radius { get ; set; }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            {
+                return Radius * Radius * Math.PI;
+            }
+        }
+        public double GetCircumference()
+        {
+            return Radius * 2  * Math.PI;
+        }
+        public double GetVolume()
+        {
+            return (4.0/3.0) * Math.PI * Math.Pow(Radius, 3);
+        }
+
+    }
+}
---
+++ CircleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CircleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle1 = new Circle(5);
+            double area1 = myCircle1.GetArea();
+            Console.WriteLine($"The area of a cirle with a radius of 5 is {area1:0.##}");
+
+            Circle myCircle2 = new Circle(6);
+            double area2 = myCircle2.GetArea();
+            Console.WriteLine($"The area of a cirle with a radius of 6 is {area2:0.##}");
+
+            double circumference = myCircle1.GetCircumference();
+            Console.WriteLine($"The circumference of a circle with a radius of 5 is {circumference:0.##}");
+            double volume = myCircle1.GetVolume();
+            Console.WriteLine($"The volume of a circle with a radius of 5 is {volume:0.##}");
+        }
+    }
+}
